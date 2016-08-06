8a90807cc495687b85e6e4ee15ac5d06285b1462
Mingchao Ji <jmc891205@gmail.com>
2016-08-06 22:28:08 +0800

    update sha256 of python34

--- python34.rb
+++ python34.rb
@@ -3,7 +3,7 @@ class Python34 < Formula
   homepage "https://www.python.org/"
   url "https://www.python.org/ftp/python/3.4.5/Python-3.4.5.tgz"
   version "3.4.5"
-  md5 "5f2ef90b1adef35a64df14d4bb7af733"
+  sha256 "997aca4dd8692f3c954658a3db11c1d0862bcbf8eadd6a164746eb33d317c034"
 
   def install
     system "./configure", "--prefix=#{prefix}"
